04231b3ac215310f8a7ecfd8daad023fffb42f6b
sreedish-ps <sreedish.ps@inmobi.com>
2014-06-04 02:22:44 +0530

    problem 2 alternate method

--- src/problem002.java
+++ src/problem002.java
@@ -6,6 +6,9 @@ public class problem002 {
     public static void main(String[] args) {
         int i=1,j=2,temp =0,sum =0;
 
+        /*
+        method 1
+         */
         while(j < 4000000) {
             if(j % 2 == 0){
                 sum+=j;
@@ -13,6 +16,34 @@ public class problem002 {
             temp = i + j;
             i = j;
             j = temp;
+        }
+        System.out.println(sum);
+
+        /*
+        method 2 without the check for fibonacci number is even or not
+        this exploits the fact that in the series
+        1 1 (2) 3 5 (8) 13 21 (34) .... every third number is even
+         */
+
+        i=1;
+        j=1;
+        temp =0;
+        sum =0;
+
+        while(j < 4000000) {
+
+            temp = i + j;
+            sum+=temp;
+            i = j;
+            j = temp;
+
+            temp = i + j;
+            i = j;
+            j= temp ;
+
+            temp = i + j;
+            i = j;
+            j= temp ;
         }
         System.out.println(sum);
     }
