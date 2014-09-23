16f609b65afadfdd2ac1b11531bcabd580c22608
sreedish-ps <sreedish.ps@inmobi.com>
2014-09-24 02:38:21 +0530

    awesome solution

--- src/Problem026.java
+++ src/Problem026.java
@@ -1,14 +1,53 @@
+import java.util.HashMap;
+
 /**
  * Created by sreedish.ps on 15/09/14.
  */
 public class Problem026 {
     public static void main(String[] args) {
+        int max =0;
+        int ans =0;
 
-        for(int i=2;i<1000;i++){
-            int j = 9;
-            while(true){
+       for(int i=2;i<1000;i++){
 
-            }
-        }
+           //filtering out terminating fractions
+           int j =i;
+           while(j % 2 == 0){
+               j = j/2;
+           }
+           while(j % 5 == 0){
+               j = j/5;
+           }
+           if(j == 1){
+               continue;
+           }
+            //finding the recurrence cycle length
+           HashMap<Integer,Integer> hs = new HashMap<Integer,Integer>();
+           int divident = 1;
+           int counter = 1;
+
+           while(true){
+
+                while(divident < i){
+                    divident*=10;
+                    counter++;
+                }
+
+
+               if(hs.containsKey(divident)){
+                   if(max < (counter - hs.get(divident))){
+                       max = counter - hs.get(divident);
+                        ans = i;
+                   }
+
+                   break;
+               }
+
+               hs.put(divident,counter);
+               divident = divident % i;
+           }
+
+       }
+        System.out.println(ans);
     }
 }
